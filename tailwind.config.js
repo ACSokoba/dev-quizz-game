62f23a89bf682b03d57dd488c72d33a539837641
ACSokoba <44870671+ACSokoba@users.noreply.github.com>
2022-05-18 03:16:08 +0200

    layout

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        mainPurple: "#190933",
+        mainOrange: "#CC5803",
+        mainBlue: "#586F7C",
+        mainGreen: "#9EBC9E",
+        mainRed: "#6A2E35",
+      },
+    },
+  },
+  plugins: [],
+};
